642e5e9d315ce969ffc8ff5f8336ab3bfc98416f
Jopapy19 <jopapy19@gmail.com>
2021-03-15 12:49:16 +0100

    Adding tensorboard_log_dir

--- utils/config.py
+++ utils/config.py
@@ -9,3 +9,4 @@ TRAINED_MODEL_DIR = os.path.join("VGGmodel", "models")
 CHECKPOINT_DIR = os.path.join("VGGmodel", "checkpoints")
 AUGMENTATION = True
 BASE_LOG_DIR = "base_log_dir"
+TENSORBOARD_ROOT_LOG_DIR = os.path.join(BASE_LOG_DIR, "tensorboard_log_dir")
